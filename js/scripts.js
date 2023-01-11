19ce6ea2b2fd743e6673a0eb0daf3107123d7aeb
skerrey <sethkerrey@gmail.com>
2023-01-10 19:23:25 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/*
+    Front End Developer Skills Test
+    Created by Seth Kerrey
+    1/4/2023
+*/
+
+
+// Anchor function
+function scrollHeader() {
+    let e = document.getElementById('header');
+    e.scrollIntoView();
+}
+
+function scrollHistory() {
+    let e = document.getElementById('history');
+    e.scrollIntoView();
+}
+
+function scrollTeam() {
+    let e = document.getElementById('team');
+    e.scrollIntoView();
+}
